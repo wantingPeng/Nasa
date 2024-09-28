25ffb8bf6cf5a85c7004f507171f805061a65ddf
wanting <wantingpeng.cici@gmail>
2024-09-28 15:29:27 +0200

    creat planets related files
    creat planets-router controller model and try fetch planets data at fontend

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,15 @@
+const http = require("http");
+const app = require("./app");
+const planetsRouter = require("./routes/planets/planets.router");
+const express = require("express");
+
+const server = http.createServer(app);
+//see app(express) as a middelware, separate middelware from server to organize better
 const PORT = process.env.PORT || 8000;
-console.log(PORT);
+
+app.use(express.json());
+app.use(planetsRouter);
+
+server.listen(PORT, () => {
+  console.log(`Listing on port ${PORT}............`);
+});
