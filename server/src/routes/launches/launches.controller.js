0fcfdb8f49a3e3e7f22788e05b10c76ba8df48ac
wanting <wantingpeng.cici@gmail>
2024-09-29 17:59:15 +0200

    list invalid action  in posting launchData

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -5,8 +5,26 @@ function getAllLaunches(req, res) {
 }
 function httpAddNewLaunch(req, res) {
   const newLaunch = req.body;
-  newLaunch.launchDate = new Date(launches.launchDate);
+
+  if (
+    !newLaunch.mission ||
+    !newLaunch.rocket ||
+    !newLaunch.launchDate ||
+    !newLaunch.destination
+  ) {
+    return res.status(400).json({
+      error: "missing required launch property",
+    });
+  }
+
+  if (isNaN(newLaunch.launchDate)) {
+    return res.status(400).json({
+      error: "invalid launch date",
+    });
+  }
+
+  newLaunch.launchDate = new Date(newLaunch.launchDate);
   addNewLaunch(newLaunch);
-  return res.status(201).json(newLaunch);
+  return res.status(201).json(newLaunch); // if no .json(newLaunch);, in postman will got no response data returned
 }
 module.exports = { getAllLaunches, httpAddNewLaunch };
