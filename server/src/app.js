26234c67cbd15747e13351893c84d777d69eecbb
wanting <wantingpeng.cici@gmail>
2024-09-29 10:42:31 +0200

    create launches model

--- server/src/app.js
+++ server/src/app.js
@@ -2,12 +2,20 @@ const express = require("express");
 const planetsRouter = require("./routes/planets/planets.router");
 const cors = require("cors");
 const app = express();
-
+const path = require("path");
+const morgan = require("morgan");
 app.use(
   cors({
     origin: "http://localhost:3000",
   })
 );
+app.use(morgan("combined"));
+
 app.use(express.json());
 app.use(planetsRouter);
+app.use(express.static(path.join(__dirname, "..", "public")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "public", "index.html"));
+});
+
 module.exports = app;
